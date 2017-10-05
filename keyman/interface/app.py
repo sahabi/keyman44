00b5401410e82ea27b6771a589e8cad5cc2781bd
sahabi <sahabi@gmail.com>
2017-10-04 23:46:38 -0500

    constructing the main app

--- keyman/interface/app.py
+++ keyman/interface/app.py
@@ -5,107 +5,27 @@ from PyQt5.QtCore import Qt
 from pycoin.key.BIP32Node import BIP32Node
 from pycoin.cmds.ku import get_entropy
 from pycoin.networks.default import get_current_netcode
-from first_page import Ui_first
 from ..encoding.encoding import get_chain_secret_pair
 import sys
-from second_page import Ui_second
 from PyQt5.QtWidgets import QTreeWidgetItem
 from pycoin.key.BIP32Node import BIP32Node
 from .keychain import KeyChain
 from .widgets import PubKeyTreeWidgetItem, PrivKeyTreeWidgetItem
+import bip44_page
+import send_page
+import rcv_page
 
 class App(object):
     def __init__(self):
-        self.app = QtWidgets.QApplication(sys.argv)
-        self.first = QtWidgets.QDialog()
-        self.second = QtWidgets.QDialog()
-        self.ui = Ui_first()
-        self.ui.setupUi(self.first)
-        self.ui.gen_key_button.clicked.connect(self.gen_seed)
-        self.ui.secret_exponent.textChanged.connect(self.detect_text)
-        self.ui.next_button.clicked.connect(self.next)
-        self.first.show()
+        self.app = QtWidgets.QApplication(sys.argv) 
+        self.main_dialog = QtWidgets.QDialog()
+        self.bip44_page = bip44_page.Ui_Dialog()
+        self.bip44_page.setupUi(self.main_dialog)
+        self.main_dialog.show()
         self.netcode = get_current_netcode()
-        self.privkeychain = KeyChain()
-        self.pubkeychain = KeyChain()
 
     def run(self):
         sys.exit(self.app.exec_())
-
-    def retranslateUi(self, first):
-        _translate = QtCore.QCoreApplication.translate
-        self.first.setWindowTitle(_translate("first", "Welcome"))
-        self.gen_seed_button.setText(_translate("first", "Generate Seed "))
-        self.next_button.setText(_translate("first", "Next"))
-
-    def next(self):
-        self.first.done(0)
-        self.start_second()
-
-    def start_second(self):
-        self.ui = Ui_second()
-        self.ui.setupUi(self.second)
-        master_node = BIP32Node(self.netcode, self.chain_code,
-                secret_exponent=int(self.secret_exponent,16))
-        master_pub_node = master_node.public_copy()
-        privkey_tree_item = PrivKeyTreeWidgetItem(key_type='Private Key',
-                level='m', key=master_node)
-        pubkey_tree_item = PubKeyTreeWidgetItem(key_type='Public Key',
-                level='M', key=master_pub_node)
-
-        self.privkeychain.append(privkey_tree_item)
-        self.pubkeychain.append(pubkey_tree_item)
-        self.ui.treeWidget.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.ui.treeWidget.customContextMenuRequested.connect(self.show_context_menu)
-        self.ui.treeWidget.addTopLevelItems([privkey_tree_item, pubkey_tree_item])
-        self.second.show()
-
-    def show_context_menu(self, pos):
-        menu = QtWidgets.QMenu()
-        menu.addAction("Rename", self.rename_key)
-        if self.pubkeychain.isSelected():
-            menu.addAction("Create PubKey", self.derive_pub_from_pub)
-        elif self.privkeychain.isSelected():
-            menu.addAction("Create PubKey", self.derive_pub_from_priv)
-            menu.addAction("Create PrivKey", self.derive_priv)
-        action = menu.exec_(self.ui.treeWidget.mapToGlobal(pos))
-
-    def rename_key(self):
-        item = self.privkeychain.get_selected_key()
-        item.setText(0,'Hi')
-
-    def derive_pub_from_priv(self, index=1):
-        parent_level = self.privkeychain.get_selected_level()
-        parent_key = self.privkeychain.get_selected_key().key
-        self._derive_pub(parent_key, parent_level, self.privkeychain)
-
-    def derive_pub_from_pub(self, index=1):
-        parent_level = self.pubkeychain.get_selected_level()
-        parent_key = self.pubkeychain.get_selected_key().key
-        self._derive_pub(parent_key, parent_level, self.pubkeychain)
-
-    def _derive_pub(self, parent_key, parent_level, keychain, index=2):
-        child_key_type = 'Public Key'
-        child_key = parent_key.subkey(as_private=False)
-        if parent_level[0] not in ['N', 'M']:
-            child_key_label = 'N({})'.format(parent_level) + '/{}'.format(index)
-        else:
-            child_key_label = parent_level + '/{}'.format(index)
-        child_tree_item = PubKeyTreeWidgetItem(child_key_type, child_key_label,
-                child_key)
-        self.pubkeychain.append(child_tree_item)
-        keychain.get_selected_key().addChild(child_tree_item)
-
-    def derive_priv(self, index=2):
-        parent_level = self.privkeychain.get_selected_level()
-        parent_key = self.privkeychain.get_selected_key().key
-        child_key_type = 'Private Key'
-        child_key_label = parent_level + '/{}'.format(index)
-        child_key = parent_key.subkey(as_private=True)
-        child_tree_item = PrivKeyTreeWidgetItem(child_key_type,
-                child_key_label, child_key)
-        self.privkeychain.append(child_tree_item)
-        self.privkeychain.get_selected_key().addChild(child_tree_item)
 
     def gen_seed(self):
         self.chain_code, self.secret_exponent = get_chain_secret_pair()
